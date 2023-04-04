1881547b63304495bad7c2de8a3d2dac112ddf78
James Carman <jcarman@callibrity.com>
2023-04-04 09:00:06 -0400

    initial version

---
+++ src/main/java/com/callibrity/hello/fluxcd/HelloFluxcdApplication.java
@@ -0,0 +1,13 @@
+package com.callibrity.hello.fluxcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloFluxcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloFluxcdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/callibrity/hello/fluxcd/HelloFluxcdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callibrity.hello.fluxcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloFluxcdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
